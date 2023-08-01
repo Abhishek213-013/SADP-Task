35ae21a12c5817322393b5ac1c4c195c18cb3ed1
Abhishek213-013 <141157999+Abhishek213-013@users.noreply.github.com>
2023-08-01 20:33:40 +0600

    Add files via upload

---
+++ EmailService.java
@@ -0,0 +1,6 @@
+public class EmailService{
+
+    public void sendEmail(String to,String subject,String body){
+        System.out.println("Email sent!");
+    }
+}
---
+++ UserAuthentication.java
@@ -0,0 +1,6 @@
+public class UserAuthentication{
+
+    public void login(String userName, String password){
+        System.out.println("User authenticated!");
+    }
+}
